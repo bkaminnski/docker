415a50934e001f03a48073e3d59673892187aac7
Bartosz Kaminski <bartosz.kaminski@zoho.com>
2018-04-14 13:44:16 +0200

    added feature to wait with netcat

--- spring-boot-service/reload.ps1
+++
@@ -1,3 +0,0 @@
-docker rmi bkaminnski/spring-boot-service
-docker build -t bkaminnski/spring-boot-service .
-docker run -it --rm --env "WAIT_FOR=urls-database;8333;READY" bkaminnski/spring-boot-service /bin/bash
