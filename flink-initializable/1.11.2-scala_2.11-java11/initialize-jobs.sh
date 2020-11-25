6154aaf5ae2eb789fd1a498b7a59b6809a902a41
Bartosz Kaminski <bartosz.kaminski@zoho.com>
2020-11-25 21:26:20 +0100

    fixed waiting for running jobs
    when 'flink run' starts in detached mode it always returns exit code 0; so here we start flink in detached mode and the list running jobs waiting for the expected status line; splitting multiple values parameter by newline character so that expected status might contain spaces

--- flink-initializable/1.11.2-scala_2.11-java11/initialize-jobs.sh
+++ flink-initializable/1.11.2-scala_2.11-java11/initialize-jobs.sh
@@ -6,16 +6,19 @@ if [ "x$INITIAL_JOBS" = "x" ]; then
   echo "No Flink jobs to initialize"
 else
   sleep $INITIAL_JOBS_DELAY
+  IFS=$'\n'
   for INITIAL_JOB in $INITIAL_JOBS; do
-    # INITIAL_JOB with pattern "MAIN_CLASS;JAR_FILE"
-    JOB_VALUES=(${INITIAL_JOB//;/ })
+    # INITIAL_JOB with pattern "MAIN_CLASS;JAR_FILE;EXPECTED_STATUS"
+    JOB_VALUES=(${INITIAL_JOB//;/$'\n'})
     MAIN_CLASS=${JOB_VALUES[0]}
     JAR_FILE="$INITIAL_JOBS_DIR/${JOB_VALUES[1]}"
-
-    echo "Trying to initialize $MAIN_CLASS in $JAR_FILE..."
-    until flink run -d -m $INITIAL_JOBS_JOBMANAGER -c $MAIN_CLASS $JAR_FILE; do
+    EXPECTED_STATUS=${JOB_VALUES[2]}
+    
+    echo "########## Trying to initialize $MAIN_CLASS in $JAR_FILE, waiting for \"$EXPECTED_STATUS\"..."
+    until flink list -r -m $INITIAL_JOBS_JOBMANAGER | grep $EXPECTED_STATUS; do
+      flink run -d -m $INITIAL_JOBS_JOBMANAGER -c $MAIN_CLASS $JAR_FILE
       sleep $INITIAL_JOBS_PERIOD
-      echo "Trying to initialize $MAIN_CLASS in $JAR_FILE..."
+      echo "########## Trying to initialize $MAIN_CLASS in $JAR_FILE, waiting for \"$EXPECTED_STATUS\"..."
     done
   done
 fi
